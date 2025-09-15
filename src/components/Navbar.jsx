cc40d501bacb4a383d1eacbccd8d48792eddea42
Satyam Pal <123195762+palsatyam877@users.noreply.github.com>
2025-09-15 13:11:58 +0530

    Updated files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
         <div className="navbar-container">
           <div className="left-part">
             <div className="logo">
-              <img src="../../public/vite.svg" alt="Brand Icon" />
+              <img src="../Vite.png" alt="Brand Icon" />
             </div>
 
             <div className="company-name">3D SLAYERS</div>
@@ -14,7 +14,7 @@ function Navbar() {
             <div className="username">SP-IN-DEMO</div>
 
             <div className="profile-icon">
-              <img src="../../public/people_11402198.png" alt="" />
+              <img src="../people_11402198.png" alt="" />
             </div>
           </div>
         </div>
